d5aea9ea5ccc432e78e392f0639ee83b603bbc87
Ametas <cherrom0708@gmail.com>
2024-11-17 22:29:43 +0300

    remove react-client-side added locations controller move locations from bot.js to json

--- bot.js
+++ bot.js
@@ -3,86 +3,13 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
 
 const token = process.env.BOT_TOKEN
-const url = process.env.APP_URL
 const bot = new TelegramBot(token, { polling: true })
+const locations = require('./assets/locations.json')
 
-const locations = {
-  'Район Бибирево': {
-    latitude: 55.894495,
-    longitude: 37.607673,
-    places: [
-      {
-        name: 'Общага',
-        latitude: 55.891132,
-        longitude: 37.616239,
-      },
-      {
-        name: 'Государственный исторический музей',
-        latitude: 55.7557,
-        longitude: 37.6173,
-      },
-      { name: 'Большой театр', latitude: 55.7602, longitude: 37.6173 },
-    ],
-  },
-  ВДНХ: {
-    latitude: 55.826398,
-    longitude: 37.637875,
-    places: [
-      {
-        name: 'Статуя «Рабочий и колхозница».',
-        latitude: 55.828165,
-        longitude: 37.64681,
-      },
-      { name: 'Музей космонавтики', latitude: 55.82271, longitude: 37.639743 },
-      {
-        name: 'Памятник В.И. Ленину на ВДНХ',
-        latitude: 55.82804,
-        longitude: 37.634734,
-      },
-      {
-        name: 'Фонтан Дружбы народов',
-        latitude: 55.82985,
-        longitude: 37.631782,
-      },
-      {
-        name: 'Ротонда, павильон 66',
-        latitude: 55.831623,
-        longitude: 37.631947,
-      },
-      {
-        name: 'РГСУ',
-        latitude: 55.834768,
-        longitude: 37.635307,
-      },
-    ],
-  },
-  'Локация 3': {
-    latitude: 54.7381,
-    longitude: 55.9939,
-    places: [
-      {
-        name: 'Национальный музей Республики Башкортостан',
-        latitude: 54.7381,
-        longitude: 55.9939,
-      },
-      { name: 'Парк Победы', latitude: 54.735, longitude: 55.991 },
-      { name: 'Уфимский кремль', latitude: 54.735, longitude: 55.965 },
-    ],
-  },
-}
-
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  const R = 6371
-  const dLat = (lat2 - lat1) * (Math.PI / 180)
-  const dLon = (lon2 - lon1) * (Math.PI / 180)
-  const a =
-    Math.sin(dLat / 2) ** 2 +
-    Math.cos(lat1 * (Math.PI / 180)) *
-      Math.cos(lat2 * (Math.PI / 180)) *
-      Math.sin(dLon / 2) ** 2
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-  return R * c
-}
+const {
+  getDistanceInKm,
+  isOnLocation,
+} = require('./controllers/locations.controller')
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id
@@ -114,12 +41,13 @@ bot.on('message', (msg) => {
     let closestDistance = Infinity
 
     for (const [locationName, coords] of Object.entries(locations)) {
-      const distance = getDistanceFromLatLonInKm(
+      const distance = getDistanceInKm(
         latitude,
         longitude,
         coords.latitude,
         coords.longitude,
       )
+      console.log(distance)
       if (distance < closestDistance) {
         closestDistance = distance
         closestLocation = locationName
@@ -174,19 +102,18 @@ bot.on('callback_query', (callbackQuery) => {
   const selectedPlace = locations[locationName].places[placeIndex]
 
   if (selectedPlace) {
-    const distanceToPlace = getDistanceFromLatLonInKm(
+    const distanceToPlace = getDistanceInKm(
       userLocation.latitude,
       userLocation.longitude,
       selectedPlace.latitude,
       selectedPlace.longitude,
     )
 
-    let responseText =
-      distanceToPlace < 0.05
-        ? 'Вы на месте!'
-        : `Вы находитесь в ${distanceToPlace.toFixed(2)} км от ${
-            selectedPlace.name
-          }.`
+    let responseText = isOnLocation(distanceToPlace, 0.05)
+      ? 'Вы на месте!'
+      : `Вы находитесь в ${distanceToPlace.toFixed(2)} км от ${
+          selectedPlace.name
+        }.`
 
     bot
       .sendMessage(bot.userData.chatId, responseText)
--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import Map from './Map.jsx'
-
-function App() {
-  return <div></div>
-  // return <Map />
-}
-
-export default App
